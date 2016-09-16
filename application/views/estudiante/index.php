183dd0dc86848dd80d9d05482cbfc2d4c2cc1419
eleonsolar <eleonsolar@gmail.com>
2016-09-15 20:39:31 -0400

    Actualización de titulo

--- application/views/estudiante/index.php
+++ application/views/estudiante/index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Student management web app</title>
+    <title>Student management web app </title>
     <link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/datatables/css/dataTables.bootstrap.css')?>" rel="stylesheet">
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
