e55ab9a69628047b1c6dc5fff1537eb85557e896
eleonsolar <eleonsolar@gmail.com>
2016-12-19 12:07:43 -0400

    Update database configuration

--- application/views/estudiante/index.php
+++ application/views/estudiante/index.php
@@ -25,7 +25,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand"  href="http://localhost/crud-demo/">Home</a>
+          <a class="navbar-brand" style="color:#fff" href="http://localhost/crud-demo/">Home</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav ">
