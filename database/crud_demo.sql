32206dea672776edbb2a168e72d5a16bb467d4ec
eleonsolar <eleonsolar@gmail.com>
2016-12-20 18:22:47 -0400

    Update backup database

--- database/crud_demo.sql
+++ database/crud_demo.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE  IF NOT EXISTS `crud_demo` /*!40100 DEFAULT CHARACTER SET big5 */;
+CREATE DATABASE  IF NOT EXISTS `crud_demo` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `crud_demo`;
--- MySQL dump 10.13  Distrib 5.6.28, for debian-linux-gnu (i686)
+-- MySQL dump 10.13  Distrib 5.5.53, for debian-linux-gnu (i686)
 --
--- Host: localhost    Database: control_estudio
+-- Host: localhost    Database: crud_demo
 -- ------------------------------------------------------
--- Server version	5.6.28-0ubuntu0.15.04.1
+-- Server version	5.5.53-0+deb8u1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -28,7 +28,7 @@ CREATE TABLE `tbl_carrera` (
   `carr_id` int(11) NOT NULL AUTO_INCREMENT,
   `carr_nombre` varchar(45) NOT NULL,
   PRIMARY KEY (`carr_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=big5;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=big5;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +37,7 @@ CREATE TABLE `tbl_carrera` (
 
 LOCK TABLES `tbl_carrera` WRITE;
 /*!40000 ALTER TABLE `tbl_carrera` DISABLE KEYS */;
-INSERT INTO `tbl_carrera` VALUES (1,'Ingenieria de Sistemas'),(2,'Sociologia'),(3,'Contabilidad');
+INSERT INTO `tbl_carrera` VALUES (1,'System engineering'),(2,'Sociology'),(3,'Accounting'),(4,'Arquitect'),(5,'Nurse'),(6,'Medical Doctor'),(7,'Policeman'),(8,'Bussinessman'),(9,'Driver');
 /*!40000 ALTER TABLE `tbl_carrera` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -57,7 +57,7 @@ CREATE TABLE `tbl_estudiante` (
   PRIMARY KEY (`estu_id`),
   KEY `carr_id` (`carr_id`),
   CONSTRAINT `tbl_estudiante_ibfk_1` FOREIGN KEY (`carr_id`) REFERENCES `tbl_carrera` (`carr_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=big5;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=big5;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,7 +66,7 @@ CREATE TABLE `tbl_estudiante` (
 
 LOCK TABLES `tbl_estudiante` WRITE;
 /*!40000 ALTER TABLE `tbl_estudiante` DISABLE KEYS */;
-INSERT INTO `tbl_estudiante` VALUES (4,'Esteban','LEON',19201202,2),(7,'Carlos','Gonzalez',123456,3),(8,'dd','dd',123344,2);
+INSERT INTO `tbl_estudiante` VALUES (4,'Esteban','Leon',19201202,2),(7,'Carlos','Gonzalez',123456,3),(9,'Ysmael','Blanco',12000311,3),(10,'Carlos','Ramos',18292111,2),(12,'Cecilio','Jimenez',16987234,1),(13,'Pedro','Perez',15030322,2),(14,'Ruben','Mejias',20929112,2),(15,'Erick','Perez',18782999,3),(16,'Carlos','Martinez',12399111,3),(17,'Silvio','Bustamante',23001922,3),(18,'Raul','Rodriguez',21122394,2),(20,'Carlos','Farias',123445,8),(22,'Claudio','Saraza',43434343,8);
 /*!40000 ALTER TABLE `tbl_estudiante` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -79,4 +79,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-07-16 18:35:35
+-- Dump completed on 2016-12-20 18:21:39
